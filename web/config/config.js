f0dde774521926b607f4617df599eff12c618d70
mxvc <410518072@qq.com>
2025-12-07 18:14:34 +0800

    init

---
+++ web/config/config.js
@@ -0,0 +1,8 @@
+import {defineConfig} from 'umi';
+import config from "@jiangood/springboot-admin-starter/config/dist/config";
+
+
+
+console.log('配置',config)
+
+export default defineConfig(config);
