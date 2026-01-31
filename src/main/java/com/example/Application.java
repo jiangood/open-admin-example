66907bbde8805dd7ebaa591ea046f0b14aef1291
jiangood <jiangood@outlook.com>
2026-01-31 17:12:56 +0800

    升级

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -1,13 +1,12 @@
 package com.example;
 
-import io.admin.BasePackage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 
-@SpringBootApplication(scanBasePackageClasses = {BasePackage.class, Application.class})
-@EntityScan(basePackageClasses =  {BasePackage.class, Application.class})
+@SpringBootApplication
+@EntityScan(basePackageClasses =  { Application.class})
 public class Application {
 
     public static void main(String[] args) {
