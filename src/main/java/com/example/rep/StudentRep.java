2f8069c30f7dfe55baa536f8bf0dff6e2bda9144
姜涛 <410518072@qq.com>
2026-05-15 20:20:48 +0800

    删除 StudentRep.java

--- src/main/java/com/example/rep/StudentRep.java
+++
@@ -1,8 +0,0 @@
-package com.example.rep;
-
-import com.example.entity.Student;
-import io.github.jiangood.openadmin.framework.data.BaseRepository;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRep extends BaseRepository<Student, String> {
-}
