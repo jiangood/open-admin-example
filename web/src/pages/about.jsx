991e1a2cf506de51e60df3e9ec3940462fb613d8
jiangood <410518072@qq.com>
2026-07-24 22:00:28 +0800

    chore: remove unused about page

--- web/src/pages/about.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import {Card} from "antd";
-import {Page} from "@jiangood/open-admin";
-
-export default function () {
-    return <Page>
-        <Card>关于示例系统</Card>
-    </Page>
-}
