274a32f001e0d8c1b2c0eaa500d8362850e878bb
jiangood <410518072@qq.com>
2026-07-24 21:59:01 +0800

    refactor: remove layouts dir, import Layouts directly from @jiangood/open-admin

--- web/src/layouts/index.jsx
+++
@@ -1,4 +0,0 @@
-import React from "react";
-import {Layouts} from "@jiangood/open-admin";
-
-export default Layouts;
--- web/src/main.jsx
+++ web/src/main.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import {createRoot} from 'react-dom/client';
 import routes from 'virtual:open-admin/routes';
-import {registerRoutes, PageLoading} from '@jiangood/open-admin';
-import Layouts from './layouts';
+import {registerRoutes, PageLoading, Layouts} from '@jiangood/open-admin';
 
 registerRoutes(routes);
 createRoot(document.getElementById('root')).render(
