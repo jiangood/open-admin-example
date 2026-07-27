34ee8769e746c6452b0e64b4016ba0d7341cd649
jiangood <410518072@qq.com>
2026-07-27 10:56:43 +0800

    refactor: use FormModal from framework for product page

--- web/src/pages/product/index.jsx
+++ web/src/pages/product/index.jsx
@@ -1,33 +1,28 @@
 import {PlusOutlined} from '@ant-design/icons'
-import {Button, Form, Input, InputNumber, Modal, Popconfirm} from 'antd'
+import {Button, Form, Input, InputNumber, Popconfirm} from 'antd'
 import React from 'react'
-import {DictUtils, FieldDictSelect, FieldUploadFile, HttpUtils, Page, PermActions, ProTable, ViewImage} from "@jiangood/open-admin";
+import {DictUtils, FieldDictSelect, FieldUploadFile, FormModal, HttpUtils, Page, PermActions, ProTable, ViewImage} from "@jiangood/open-admin";
 
 export default class extends React.Component {
 
-    state = {
-        formValues: {},
-        formOpen: false
-    }
+    state = {}
 
-    formRef = React.createRef()
+    modalRef = React.createRef()
     tableRef = React.createRef()
 
     handleAdd = () => {
-        this.setState({formOpen: true, formValues: {}})
+        this.modalRef.current.open({})
     }
 
     handleEdit = record => {
-        this.setState({formOpen: true, formValues: record})
+        this.modalRef.current.open(record)
     }
 
-    onFinish = values => {
+    onFinish = async values => {
         const isNew = !values.id;
         const url = isNew ? 'admin/product/create' : 'admin/product/update';
-        HttpUtils.post(url, values).then(rs => {
-            this.setState({formOpen: false})
-            this.tableRef.current.reload()
-        })
+        await HttpUtils.post(url, values)
+        this.tableRef.current.reload()
     }
 
     handleDelete = record => {
@@ -93,39 +88,28 @@ export default class extends React.Component {
                 </Form.Item>
             </ProTable>
 
-            <Modal title='商品'
-                   open={this.state.formOpen}
-                   onOk={() => this.formRef.current.submit()}
-                   onCancel={() => this.setState({formOpen: false})}
-                   destroyOnHidden
-            >
-                <Form ref={this.formRef} labelCol={{flex: '100px'}}
-                      initialValues={this.state.formValues}
-                      onFinish={this.onFinish}
-                >
-                    <Form.Item name='id' noStyle></Form.Item>
+            <FormModal ref={this.modalRef} title='商品' onFinish={this.onFinish}>
 
-                    <Form.Item label='商品名称' name='name' rules={[{required: true}]}>
-                        <Input/>
-                    </Form.Item>
+                <Form.Item label='商品名称' name='name' rules={[{required: true}]}>
+                    <Input/>
+                </Form.Item>
 
-                    <Form.Item label='价格' name='price'>
-                        <InputNumber style={{width: '100%'}} min={0} precision={2}/>
-                    </Form.Item>
+                <Form.Item label='价格' name='price'>
+                    <InputNumber style={{width: '100%'}} min={0} precision={2}/>
+                </Form.Item>
 
-                    <Form.Item label='描述' name='description'>
-                        <Input.TextArea rows={3}/>
-                    </Form.Item>
+                <Form.Item label='描述' name='description'>
+                    <Input.TextArea rows={3}/>
+                </Form.Item>
 
-                    <Form.Item label='状态' name='status'>
-                        <FieldDictSelect typeCode='product_status'/>
-                    </Form.Item>
+                <Form.Item label='状态' name='status'>
+                    <FieldDictSelect typeCode='product_status'/>
+                </Form.Item>
 
-                    <Form.Item label='图片' name='image'>
-                        <FieldUploadFile accept="image/*" maxCount={1}/>
-                    </Form.Item>
-                </Form>
-            </Modal>
+                <Form.Item label='图片' name='image'>
+                    <FieldUploadFile accept="image/*" maxCount={1}/>
+                </Form.Item>
+            </FormModal>
         </Page>
     }
 }
