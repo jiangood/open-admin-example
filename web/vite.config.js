0504cc26802bdb68ef08c310915135c479c7c846
jiangood <410518072@qq.com>
2026-07-28 16:39:01 +0800

    config(web): 更新开发环境配置和端口设置
    - 将前端端口从8090更改为3000
    - 添加SERVER_PORT环境变量配置后端端口为8080
    - 重命名VITE_SERVLET_CONTEXT为VITE_SERVER_SERVLET_CONTEXT_PATH
    - 在vite.config.js中更新环境变量读取逻辑
    - 添加控制台日志显示端口和上下文配置信息
    - 移除重复的resolve和optimizeDeps配置项

--- web/vite.config.js
+++ web/vite.config.js
@@ -2,18 +2,19 @@ import {defineConfig, loadEnv} from 'vite';
 import react from '@vitejs/plugin-react';
 import openAdmin from '@jiangood/open-admin/vite-plugin';
 
+
 export default defineConfig(({mode, command}) => {
     const env = loadEnv(mode, process.cwd(), '');
-    const servletContext = env.VITE_SERVLET_CONTEXT || '/example';
-    const serverPort = env.SERVER_PORT || '8080';
+    const servletContext = env.VITE_SERVER_SERVLET_CONTEXT_PATH;
+    const serverPort = env.SERVER_PORT ;
+    const port = Number(env.PORT);
+    console.log('前端端口' + port +',后端端口' + serverPort +',请求上下文' + servletContext)
 
     return {
         plugins: [react(), openAdmin()],
         base: command === 'build' ? './' : '/',
-        resolve: {dedupe: ['react', 'react-dom']},
-        optimizeDeps: {exclude: ['@jiangood/open-admin']},
         server: {
-            port: env.PORT ? Number(env.PORT) : 8090,
+            port: port,
             proxy: {
                 [servletContext]: {
                     target: `http://127.0.0.1:${serverPort}`,
